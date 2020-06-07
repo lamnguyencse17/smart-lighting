565215f5041228ed643fdc1977d05f1dcce0cef0
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-06-07 17:41:07 +0700

    Temp Commit #2

--- src/client/Dashboard/AreaView.js
+++ src/client/Dashboard/AreaView.js
@@ -9,12 +9,31 @@ class AreaView extends Component {
     this.props.getArea(this.props.match.params.id);
   }
   render() {
-    let { name, devices, sensors } = this.props;
+    let { areaName, devices, sensors } = this.props;
     return (
       <div className="dashboard">
         <div className="areaview">
-          <div className="title">{name}</div>
-          <div className="sensor-box"></div>
+          <div className="title">{areaName}</div>
+          {Object.keys(sensors).map((index) => {
+            let sensorName = sensors[index].name;
+            let readings = sensors[index].readings;
+            return (
+              <div className="sensor-box" key={index}>
+                <div className="title">{sensorName}</div>
+                <div className="history">
+                  {readings.map((reading) => {
+                    let newDate = new Date(reading.date);
+                    return (
+                      <div key={reading._id}>
+                        {`${newDate.getHours()}:${newDate.getMinutes()}`}:{" "}
+                        {reading.value}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
@@ -23,7 +42,7 @@ class AreaView extends Component {
 
 function mapStateToProps(state) {
   return {
-    name: state.area.name,
+    areaName: state.area.name,
     devices: state.area.devices,
     sensors: state.area.sensors,
   };
