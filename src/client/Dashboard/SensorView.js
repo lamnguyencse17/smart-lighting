42a616710a332dac6d96320007ae38e4efc29169
tri <tri.trancongtoan1999@gmail.com>
2020-06-07 22:46:42 +0700

    SensorPanel Implementation in SensorView

--- src/client/Dashboard/SensorView.js
+++ src/client/Dashboard/SensorView.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { getSensor } from "../actions/sensor";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import SensorPanel from "./SensorView/SensorPanel";
 
 class SensorView extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getSensor(this.props.match.params.id);
+  }
   state = {
     latestReadings: "",
     history: [],
@@ -11,9 +19,9 @@ class SensorView extends Component {
     return (
       <div className="sensor-view">
         <div className="sensor-view-content">
-          <div className="row1">      
+          <div className="row1">
             <div className="bedroom-light">BEDROOM LIGHT 1</div>
-            <a href="#" className="edit">EDIT</a>
+            <a href="#" className="edit">EDIT</a>  
           </div>
           <SensorPanel/>
         </div>
@@ -22,4 +30,19 @@ class SensorView extends Component {
   }
 }
 
-export default SensorView;
+function mapStateToProps(state) {
+  return {
+    _id: state.sensor._id,
+    name: state.sensor.name,
+    device_id: state.sensor.device_id,
+    readings: state.sensor.readings,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getSensor }, dispatch);
+}
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(SensorView)
+);
