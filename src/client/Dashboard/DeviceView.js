62ada6db56197b4b75d0bdb3d0cebaef55ac0fd7
tri <tri.trancongtoan1999@gmail.com>
2020-06-07 16:38:27 +0700

    device-view implementation

--- src/client/Dashboard/DeviceView.js
+++ src/client/Dashboard/DeviceView.js
@@ -3,8 +3,37 @@ import React, { Component } from "react";
 class DeviceView extends Component {
   render() {
     return (
-      <div>
-        <h1>Device {this.props.match.params.name}</h1>
+      <div class="device-view">
+        <div class="device-view-content">
+          <div class="row1">      
+            <div class="bedroom-light">BEDROOM LIGHT 1</div>
+            <div class="edit">EDIT</div>
+          </div>
+          <div class="device-content">
+            <div class="device-status-container">
+              <span class="device-status-title"> status</span><br></br>
+              <span class="device-status">ON</span>
+            </div>
+            <div class="device-toggle">
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span>
+              </label>
+            </div>
+            <span class="device-history-title">HISTORIES</span>
+            <div class="device-history-content">
+              <ul>
+                <li>
+                  9AM - 21/05/2020: Turned on - Manual
+                </li>
+                <li>
+                  12AM - 21/05/2020: Turned off - Automated
+                </li>
+              </ul>
+            </div>
+            <button class="add-device-schedule">New Schedule</button>
+          </div>
+        </div>
       </div>
     );
   }
