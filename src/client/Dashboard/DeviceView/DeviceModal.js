5caa2985f41679c585b04015357632cf860f8b9f
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-06-13 00:36:40 +0700

    Updating Package.json
    File Creating For More Work Incoming

---
+++ src/client/Dashboard/DeviceView/DeviceModal.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DeviceModal extends Component {
+  render() {
+    return <div>{/* This is for Adding New Schedule */}</div>;
+  }
+}
+
+export default DeviceModal;
