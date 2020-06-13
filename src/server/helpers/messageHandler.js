d77b686ebbe6bf3180a61d8ae779a51fb4acb646
tri <tri.trancongtoan1999@gmail.com>
2020-06-13 16:03:37 +0700

    Json format in action and messageHandler

--- src/server/helpers/messageHandler.js
+++ src/server/helpers/messageHandler.js
@@ -3,12 +3,12 @@ import deviceSchema from "../models/devices";
 
 const messageHandler = (message) => {
   message = JSON.parse(message);
-  let { device_id, value } = message;
+  let { device_id, value, isOn} = message;
   // TODO: Update this to normal format
   if (device_id == "LIGHT") {
     sensorSchema.updateSensor({ device_id, value });
   } else {
-    deviceSchema.updateDevice({ device_id, value });
+    deviceSchema.updateDevice({ device_id, value, isOn});
   }
 };
 
--- src/server/routes/actions/actions.js
+++ src/server/routes/actions/actions.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post("/sendCommand", async (req, res) => {
   publishTo(req.body);
   //TODO:  convert to the format
-  let { device_id, value } = req.body;
-  let returnedValue = await deviceModel.updateToDeviceId(device_id, value);
+  let { device_id, value, isOn} = req.body;
+  let returnedValue = await deviceModel.updateToDeviceId(device_id, value, isOn);
   res.status(200).json(returnedValue);
 });
 
