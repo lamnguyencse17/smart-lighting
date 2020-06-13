a91fdf55b106887bd13e1838dfaf2e6de4242f40
tri <tri.trancongtoan1999@gmail.com>
2020-06-13 16:39:43 +0700

    Change Json publish to teacher's format

--- src/server/routes/actions/actions.js
+++ src/server/routes/actions/actions.js
@@ -5,9 +5,12 @@ import { publishTo } from "../../helpers/mqtt";
 const router = express.Router();
 
 router.post("/sendCommand", async (req, res) => {
-  publishTo(req.body);
-  //TODO:  convert to the format
   let { device_id, value, isOn} = req.body;
+  let values = [isOn,value];
+  publishTo({
+    device_id,
+    values
+  });
   let returnedValue = await deviceModel.updateToDeviceId(device_id, value, isOn);
   res.status(200).json(returnedValue);
 });
