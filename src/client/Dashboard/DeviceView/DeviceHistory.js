4b93dc97863407bdd76007766b07b6c07f1f5d1d
Nguyen Quang Lam <46278271+lamnguyencse17@users.noreply.github.com>
2020-06-17 20:19:39 +0700

    Implement #36 fix, Update Package, Remove Unused Code (#38)

--- src/client/Dashboard/DeviceView/DeviceHistory.js
+++ src/client/Dashboard/DeviceView/DeviceHistory.js
@@ -8,27 +8,27 @@ class DeviceHistory extends Component {
   render() {
     let { history } = this.props;
     return (
-        <div className="device-history-container">
-            <span className="device-history-title">HISTORY</span>
-            <div className="device-history-content">
-              <ul>
-                {Object.keys(history).map((index) => {
-                  let device = history[index];
-                  let date = new Date(device.date);
-                  return (
-                    <li className="device-history-item" key={index}>
-                      {`${date.getUTCHours()}:${date.getUTCMinutes()} - ${date.getUTCDate()}/${
-                        date.getUTCMonth() + 1
-                      }/${date.getFullYear()}`}
-                      <br></br>
-                      {device.value == 2 ? "Turned On" : "Turned OFF"}
-                      <br></br>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+      <div className="device-history-container">
+        <span className="device-history-title">HISTORY</span>
+        <div className="device-history-content">
+          <ul>
+            {Object.keys(history).map((index) => {
+              let device = history[index];
+              let date = new Date(device.date);
+              return (
+                <li className="device-history-item" key={index}>
+                  {`${date.getUTCHours()}:${date.getUTCMinutes()} - ${date.getUTCDate()}/${
+                    date.getUTCMonth() + 1
+                  }/${date.getFullYear()}`}
+                  <br></br>
+                  {device.value > 0 ? "Turned On" : "Turned OFF"}
+                  <br></br>
+                </li>
+              );
+            })}
+          </ul>
         </div>
+      </div>
     );
   }
 }
