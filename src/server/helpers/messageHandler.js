07b97babecd8857f8d8eee551cca7157edb75e79
tri <tri.trancongtoan1999@gmail.com>
2020-06-20 22:19:52 +0700

    #47 : added executeCondition

--- src/server/helpers/messageHandler.js
+++ src/server/helpers/messageHandler.js
@@ -1,6 +1,6 @@
 import sensorModel from "../models/sensors";
-import { publishTo } from "./mqtt";
-import deviceModel from "../models/devices";
+import executeCondition from "./executeCondition";
+
 
 const messageHandler = async (message) => {
   message = JSON.parse(message);
@@ -9,31 +9,7 @@ const messageHandler = async (message) => {
   let value = parseInt(values[0]);
   if (device_id == "Light") {
     sensorModel.updateSensor({ device_id, value });
-    let result = await sensorModel.getConditionsByDeviceId(device_id);
-    let willExecute = false;
-    result.conditions.forEach(async (condition) => {
-      if (condition.comparison == 0) {
-        willExecute = value == condition.sensorValue;
-      } else if (condition.comparison == 1) {
-        willExecute = value > condition.sensorValue;
-      } else {
-        willExecute = value < condition.sensorValue;
-      }
-      if (willExecute) {
-        let targetDevice = await deviceModel.readDeviceById(condition.device);
-        publishTo([
-          {
-            device_id: targetDevice.device_id,
-            values: [condition.isOn ? "1" : "0", `${condition.value}`],
-          },
-        ]);
-        await deviceModel.updateToDeviceId(
-          targetDevice.device_id,
-          condition.value,
-          condition.isOn
-        );
-      }
-    });
+    executeCondition(device_id,values)
   }
 };
 
