58df0d46e64351436a0f546300ea885849b480c4
Nguyen Quang Lam <46278271+lamnguyencse17@users.noreply.github.com>
2020-06-21 12:43:38 +0700

    Implement Logic for ErrorModal from issue #56 PR #60 (#61)

--- src/client/actions/types.js
+++ src/client/actions/types.js
@@ -3,3 +3,5 @@ export const GET_AREA = "GET_AREA";
 export const GET_DEVICE = "GET_DEVICE";
 export const ADJUST_DEVICE = "ADJUST_DEVICE";
 export const GET_SENSOR = "GET_SENSOR";
+export const SET_ERROR = "SET_ERROR";
+export const CLOSE_ERROR = "CLOSE_ERROR";
