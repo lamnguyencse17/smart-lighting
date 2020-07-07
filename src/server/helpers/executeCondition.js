e04ca4ae10e490e5bf21c2f0a807cef06411df22
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-07-07 14:38:28 +0700

    Fix mongoose populate in #48 (untested)

--- src/server/helpers/executeCondition.js
+++ src/server/helpers/executeCondition.js
@@ -3,32 +3,32 @@ import { publishTo } from "./mqtt";
 import deviceModel from "../models/devices";
 
 const executeCondition = async (device_id, values) => {
-    let value = parseInt(values[0]);
-    let result = await sensorModel.getConditionsByDeviceId(device_id);
-    let willExecute = false;
-    result.conditions.forEach(async (condition) => {
-      if (condition.comparison == 0) {
-        willExecute = value == condition.sensorValue;
-      } else if (condition.comparison == 1) {
-        willExecute = value > condition.sensorValue;
-      } else {
-        willExecute = value < condition.sensorValue;
-      }
-      if (willExecute) {
-        let targetDevice = await deviceModel.readDeviceById(condition.device);
-        publishTo([
-          {
-            device_id: targetDevice.device_id,
-            values: [condition.isOn ? "1" : "0", `${condition.value}`],
-          },
-        ]);
-        await deviceModel.updateToDeviceId(
-          targetDevice.device_id,
-          condition.value,
-          condition.isOn
-        );
-      }
-    });
+  let value = parseInt(values[0]);
+  let result = await sensorModel.getConditionsByDeviceId(device_id);
+  let willExecute = false;
+  result.conditions.forEach(async (condition) => {
+    if (condition.comparison == 0) {
+      willExecute = value == condition.sensorValue;
+    } else if (condition.comparison == 1) {
+      willExecute = value > condition.sensorValue;
+    } else {
+      willExecute = value < condition.sensorValue;
+    }
+    if (willExecute) {
+      let targetDevice = condition.device.device_id;
+      publishTo([
+        {
+          device_id: targetDevice.device_id,
+          values: [condition.isOn ? "1" : "0", `${condition.value}`],
+        },
+      ]);
+      await deviceModel.updateToDeviceId(
+        targetDevice.device_id,
+        condition.value,
+        condition.isOn
+      );
+    }
+  });
 };
 
 export default executeCondition;
--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ import conditionModel from "./models/conditions";
 import deviceModel from "./models/devices";
 import areaModel from "./models/areas";
 import { startAgenda, stopAgenda, setAgenda } from "./helpers/scheduler";
+import sensorModel from "./models/sensors";
 
 const data_uri =
   "mongodb+srv://tri:team2447@cluster0-wrndr.azure.mongodb.net/smart-lighting?retryWrites=true&w=majority";
