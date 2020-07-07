3018bf653d5e9c66cea67e2bae0f55b73f9afc6f
Dao Trong Tien <tien.dao1021999@hcmut.edu.vn>
2020-07-07 15:38:50 +0700

    convertTime

--- src/server/helpers/timeConverter.js
+++ src/server/helpers/timeConverter.js
@@ -1,5 +1,17 @@
+import { string } from "prop-types";
+
 const timeConverter = (time) => {
   //TODO
+  /*
+  let atPre = time.split("T");
+  let date = atPre[0].split("-");
+  let hourZone=atPre[1].split("+");
+  let hour =hourZone[0].split(":");
+  let zone = hourZone[1].split(":");
+  */
+ let d= new Date(time);
+
+  return "in "+(d.getTime()-Date.now())+" seconds";
 };
 
 export default timeConverter;
