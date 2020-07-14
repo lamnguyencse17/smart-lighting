999a42b163b2a1e89d20720e557d9779bd021e8c
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-07-14 20:59:15 +0700

    Rebuild with updates, fix direct URL

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const compression = require("compression");
 
 const app = express();
 
-
 app.use(cors());
 app.use(compression());
 app.use(express.static("public"));
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("server started on port 8080");
